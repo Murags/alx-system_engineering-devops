a67353c429ccb44e0ab4b454d044a39670b77b0e
Dennis Murage <dmurage140@gmail.com>
2023-03-26 18:54:57 +0300

    Task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# server configuration file changes
+
+exec { 'ssh_login':
+  command => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  path    => '/usr/bin/'
+}
