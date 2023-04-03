c29c732701466dfe8b100d21008e345aa4da6b7a
Dennis Murage <dmurage140@gmail.com>
2023-04-03 15:10:35 +0300

    Error Handle

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,18 @@
-# Puppet manifest to install nginx and add custom header
+# install nginx and add custom header
 
 exec { 'update':
   command  => 'sudo apt -y update',
   provider => 'shell',
-  before   => Exec['nginx'],
 }
 
 exec { 'nginx':
   command  => 'sudo apt -y install nginx',
   provider => 'shell',
-  before   => Exec['config'],
 }
 
 exec { 'config':
   command  => 'sudo sed -i "/listen 80 default_server;/ a     add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
   provider => 'shell',
-  before   => Exec['restart'],
 }
 
 exec { 'restart':
