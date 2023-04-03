f62cc1206b63df54e412370a6ff4eccf30cd8deb
Dennis Murage <dmurage140@gmail.com>
2023-04-03 15:08:34 +0300

    Error Handle

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,7 +13,7 @@ exec { 'nginx':
 }
 
 exec { 'config':
-  command  => 'sudo sed -i "/listen 80 default_server;/ a \\t add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "/listen 80 default_server;/ a     add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
   provider => 'shell',
   before   => Exec['restart'],
 }
