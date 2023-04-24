95d2aa115b2f1facf627c66a30dc8f5b9ef2cefe
Dennis Murage <dmurage140@gmail.com>
2023-04-24 16:46:54 +0300

    Added 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that exports to csv file
+    """
+
+import csv
+from requests import get
+from sys import argv
+
+
+def main():
+    task_res = get("https://jsonplaceholder.typicode.com/todos")
+    user_res = get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
+    employee_name = user_res.json().get("username")
+    filename = argv[1] + ".csv"
+
+    with open(filename, "w", newline='') as csvfile:
+        for task in task_res.json():
+            if task.get("userId") == int(argv[1]):
+                if task.get("completed"):
+                    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+                    writer.writerow([argv[1], employee_name, task.get(
+                        "completed"), task.get("title")])
+
+
+if __name__ == "__main__":
+    main()
